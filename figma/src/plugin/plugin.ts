4bf44cddbaf8b55f27f5280e2e47f80556bf7795
seville <amirrezasalimi0@gmail.com>
2024-06-20 17:32:59 +0330

    feat: generate palette logic

--- figma/src/plugin/plugin.ts
+++ figma/src/plugin/plugin.ts
@@ -1,26 +1,17 @@
 import * as Networker from "monorepo-networker";
 import { initializeNetwork } from "@common/network/init";
 import { NetworkSide } from "@common/network/sides";
-import { NetworkMessages } from "@common/network/messages";
 
 async function bootstrap() {
   initializeNetwork(NetworkSide.PLUGIN);
-
 
   if (figma.editorType === "figma") {
     figma.showUI(__html__, {
       width: 400,
       height: 680,
-      title: "Shades!",
+      title: "Shades - Ai color palette generator",
     });
-  } else if (figma.editorType === "figjam") {
-    /*    figma.showUI(__html__, {
-          width: 689,
-          height: 1397,
-          title: "My FigJam Plugin!",
-        }); */
   }
-
   console.log("Bootstrapped @", Networker.Side.current.getName());
 }
 
