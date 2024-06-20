439a89a6b3d1143c2e1080dd55fc7f9aa9ab051b
seville <amirrezasalimi0@gmail.com>
2024-06-20 23:09:36 +0330

    chore: update figma manifest

--- figma/figma.manifest.ts
+++ figma/figma.manifest.ts
@@ -1,7 +1,7 @@
 // https://www.figma.com/plugin-docs/manifest/
 export default {
   name: "shades",
-  id: "12228526465750",
+  id: "1385705522724184971",
   api: "1.0.0",
   main: "plugin.js",
   ui: "index.html",
@@ -12,6 +12,6 @@ export default {
   permissions: ["currentuser"],
   networkAccess: {
     allowedDomains: ["https://shades.toolstack.run/"],
-    devAllowedDomains: ["http://localhost:3000/","https://shades.toolstack.run/"],
+    devAllowedDomains: ["http://localhost:3000/", "https://shades.toolstack.run/"],
   },
 };
