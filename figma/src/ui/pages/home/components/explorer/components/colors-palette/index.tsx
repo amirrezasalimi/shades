28445a75625162237a438d5a09e34325797befff
seville <amirrezasalimi0@gmail.com>
2024-07-14 01:10:01 +0330

    improve

--- figma/src/ui/pages/home/components/explorer/components/colors-palette/index.tsx
+++ figma/src/ui/pages/home/components/explorer/components/colors-palette/index.tsx
@@ -11,6 +11,7 @@ import useFork from "@ui/pages/home/hooks/fork";
 import Shades from "./components/shades";
 import Setting from "./components/setting-bottom-sheet";
 import { TOOLSTACK } from "@ui/shared/constants/constants";
+import clsx from "clsx";
 
 interface Toggle {
   isOpen: boolean;
@@ -21,7 +22,7 @@ interface Toggle {
 const ColorPalette: FC<Toggle> = ({
   paletteId,
   isOpen,
-  showModal = () => {},
+  showModal,
 }) => {
   const forker = useFork();
   const { data, isLoading, refetch } = useQuery({
@@ -57,64 +58,67 @@ const ColorPalette: FC<Toggle> = ({
   };
   return (
     <>
-      {isOpen && (
-        <div className="top-0 right-0 left-0 z-20 fixed bg-white h-screen overflow-y-auto">
-          {isLoading ? (
-            <div className="absolute inset-0 flex justify-center items-center bg-white bg-opacity-90 w-full h-full">
-              <div className="w-10 h-10">
-                <Spinner />
+      <div
+        className={clsx(
+          "top-0 right-0 left-0 z-20 fixed bg-white h-screen overflow-y-auto transition-all duration-300",
+          isOpen ? "w-full" : "w-0"
+        )}
+      >
+        {isLoading ? (
+          <div className="absolute inset-0 flex justify-center items-center bg-white bg-opacity-90 w-full h-full">
+            <div className="w-10 h-10">
+              <Spinner />
+            </div>
+          </div>
+        ) : (
+          <div className="mb-20">
+            <div className="top-0 right-0 left-0 fixed flex justify-between items-center border-gray-100 bg-white bg-opacity-80 backdrop-blur-xl border-b h-20">
+              <div className="flex items-center space-x-4 ml-6">
+                <div
+                  className="cursor-pointer"
+                  onClick={() => showModal(false)}
+                >
+                  <img width={24} src={angleLeft} alt="" />
+                </div>
+                <p className="max-w-56 text-xl truncate">{data?.prompt}</p>
+              </div>
+              <a
+                href={`${TOOLSTACK}/p/${paletteId}`}
+                target="_blank"
+                className="mr-6"
+              >
+                <div className="flex justify-center items-center border-gray-300 border rounded-2xl w-14 h-14 cursor-pointer">
+                  <img width={22} src={info} alt="" />
+                </div>
+              </a>
+            </div>
+
+            <div className="mx-6 mt-4 pt-20 h-full">
+              <Shades data={data} />
+            </div>
+
+            <div className="bottom-5 left-1/2 fixed flex items-center rounded-2xl w-[calc(100%_-_48px)] h-[50px] -translate-x-1/2 overflow-hidden">
+              <div
+                onClick={fork}
+                className="flex justify-center items-center space-x-2 bg-gradient-to-r from-primary to-[#EC681D] w-[calc(100%_-_50px)] h-full text-white cursor-pointer"
+              >
+                <img width={22} src={figma} alt="" />
+                <span>Import to figma</span>
+              </div>
+              <div
+                className="flex justify-center items-center border-gray-100 bg-white border rounded-r-2xl w-[50px] h-full cursor-pointer"
+                onClick={() => setToggleBottomSheet(true)}
+              >
+                <img width={24} src={settings} alt="" />
               </div>
             </div>
-          ) : (
-            <div className="mb-20">
-              <div className="top-0 right-0 left-0 fixed flex justify-between items-center border-gray-100 bg-white bg-opacity-80 backdrop-blur-xl border-b h-20">
-                <div className="flex items-center space-x-4 ml-6">
-                  <div
-                    className="cursor-pointer"
-                    onClick={() => showModal(false)}
-                  >
-                    <img width={24} src={angleLeft} alt="" />
-                  </div>
-                  <p className="max-w-56 text-xl truncate">{data?.prompt}</p>
-                </div>
-                <a
-                  href={`${TOOLSTACK}/p/${paletteId}`}
-                  target="_blank"
-                  className="mr-6"
-                >
-                  <div className="flex justify-center items-center border-gray-300 border rounded-2xl w-14 h-14 cursor-pointer">
-                    <img width={22} src={info} alt="" />
-                  </div>
-                </a>
-              </div>
-
-              <div className="mx-6 mt-4 pt-20 h-full">
-                <Shades data={data} />
-              </div>
-
-              <div className="bottom-5 left-1/2 fixed flex items-center rounded-2xl w-[calc(100%_-_48px)] h-[50px] -translate-x-1/2 overflow-hidden">
-                <div
-                  onClick={fork}
-                  className="flex justify-center items-center space-x-2 bg-gradient-to-r from-primary to-[#EC681D] w-[calc(100%_-_50px)] h-full text-white cursor-pointer"
-                >
-                  <img width={22} src={figma} alt="" />
-                  <span>Import to figma</span>
-                </div>
-                <div
-                  className="flex justify-center items-center border-gray-100 bg-white border rounded-r-2xl w-[50px] h-full cursor-pointer"
-                  onClick={() => setToggleBottomSheet(true)}
-                >
-                  <img width={24} src={settings} alt="" />
-                </div>
-              </div>
-              <Setting
-                isOpen={toggleBottomSheet}
-                showBottomSheet={setToggleBottomSheet}
-              />
-            </div>
-          )}
-        </div>
-      )}
+            <Setting
+              isOpen={toggleBottomSheet}
+              showBottomSheet={setToggleBottomSheet}
+            />
+          </div>
+        )}
+      </div>
     </>
   );
 };
