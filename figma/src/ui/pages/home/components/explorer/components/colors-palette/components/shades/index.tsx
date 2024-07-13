8223967e98b32ccc293a95444ac6141a916f2d9a
seville <amirrezasalimi0@gmail.com>
2024-07-14 01:18:12 +0330

    chore: Refactor Shades component styling for better readability and maintainability

--- figma/src/ui/pages/home/components/explorer/components/colors-palette/components/shades/index.tsx
+++ figma/src/ui/pages/home/components/explorer/components/colors-palette/components/shades/index.tsx
@@ -27,17 +27,17 @@ const Shades = ({ data }: { data: PaletteFull | undefined }) => {
             const lengthShades = Object.keys(shadesColor).length;
 
             return (
-              <div className="flex flex-col border-t border-gray-100 first:border-t-0 first:pt-0 first:mt-0 pt-3 mt-3 last:pb-24">
-                <div className="flex items-center justify-between">
-                  <div className="text-lg font-semibold text-[#191919]">
+              <div className="flex flex-col border-gray-100 mt-3 first:mt-0 pt-3 first:pt-0 last:pb-24 border-t first:border-t-0">
+                <div className="flex justify-between items-center">
+                  <div className="font-semibold text-[#191919] text-lg">
                     {fullColors && fullColors[key].name}
                   </div>
-                  <div className="text-sm flex items-center space-x-3">
+                  <div className="flex items-center space-x-3 text-sm">
                     <span className="text-gray-500">{lengthShades} Colors</span>
                     <div className="text-[#46B235]">{key}</div>
                   </div>
                 </div>
-                <div className="grid grid-cols-5 gap-2 mt-5">
+                <div className="gap-2 grid grid-cols-5 mt-5">
                   {Object.keys(shadesColor).map((keyShade) => {
                     const keyShadeColor = shadesColor[keyShade];
                     return (
@@ -46,15 +46,15 @@ const Shades = ({ data }: { data: PaletteFull | undefined }) => {
                           style={{
                             background: `${keyShadeColor}`,
                           }}
-                          className="w-16 h-16 rounded-xl"
-                        ></div>
+                          className="rounded-xl w-16 h-16"
+                        />
                         <span
-                          className="text-[#191919] text-xs font-medium"
+                          className="font-medium text-[#191919] text-xs"
                         >
                           {keyShade}
                         </span>
-                        <span className="text-gray-500 text-xs font-light">
-                          {fullColors[key].hex}
+                        <span className="font-light text-gray-500 text-xs">
+                          {keyShadeColor}
                         </span>
                       </div>
                     );
