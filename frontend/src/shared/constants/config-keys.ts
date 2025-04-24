a31e3213ca2412ec33bcefce99d174e67b2bfae9
seville <amirrezasalimi0@gmail.com>
2025-04-24 21:43:38 +0330

    fix json bug & dynamic prompt

--- frontend/src/shared/constants/config-keys.ts
+++ frontend/src/shared/constants/config-keys.ts
@@ -1,5 +1,6 @@
 export const CONFIG_KEYS = {
-    SITE_CREATE_PALETTE_LIMIT: "site_create_palette_limit",
-    FIGMA_CREATE_PALETTE_LIMIT: "figma_create_palette_limit",
-    AI_MODEL: "ai_model"
-}
+  SITE_CREATE_PALETTE_LIMIT: "site_create_palette_limit",
+  FIGMA_CREATE_PALETTE_LIMIT: "figma_create_palette_limit",
+  AI_MODEL: "ai_model",
+  AI_PROMPT: "ai_prompt",
+};
--- frontend/src/shared/utils/ai-create-palette.ts
+++ frontend/src/shared/utils/ai-create-palette.ts
@@ -3,30 +3,6 @@ import Decimal from "decimal.js";
 import Config from "./pb/config";
 import { CONFIG_KEYS } from "../constants/config-keys";
 
-const makePrompt = (prompt: string) => {
-  return `You are an expert UI/UX designer with a deep understanding of color theory and the ability to create stunning color palettes. Your task is to generate a color palette based on the following input:
-"${prompt}"
-
-Requirements:
-- Every color must be a valid hex code in the #RRGGBB format (include the '#' symbol).
-- All selected colors must be exclusively from the 500 shade.
-
-Output:
-Return a JSON object exactly in the following structure, with no additional commentary:
-{
-  "primary": "#hexcolor",
-  "secondary": "#hexcolor",
-  "neutral": "#hexcolor",
-  "success": "#hexcolor",
-  "error": "#hexcolor",
-  "warning": "#hexcolor",
-  "info": "#hexcolor",
-  "background": "#hexcolor",
-  "text": "#hexcolor",
-  "description": "A brief paragraph (under 50 words) explaining the rationale behind the palette."
-}`;
-};
-
 const aiCreatePalette = async (prompt: string) => {
   let ai_model: {
     endpoint: string;
@@ -35,8 +11,10 @@ const aiCreatePalette = async (prompt: string) => {
     temperature: number;
     per_1m: string; // ex: 1.5,2
   } | null;
+  let ai_base_prompt: string;
   try {
     const ai_config = (await Config.get(CONFIG_KEYS.AI_MODEL)) as string;
+    ai_base_prompt = (await Config.get(CONFIG_KEYS.AI_PROMPT)) as string;
     ai_model = JSON.parse(ai_config);
     if (!ai_model) {
       throw "Ai Service Unavailable";
@@ -49,7 +27,7 @@ const aiCreatePalette = async (prompt: string) => {
     baseURL: ai_model.endpoint,
   });
 
-  const ai_prompt = makePrompt(prompt);
+  const ai_prompt = ai_base_prompt.replaceAll("{prompt}", prompt);
   const ai_res = await openAi.chat.completions.create({
     model: ai_model.name,
     messages: [
@@ -60,7 +38,9 @@ const aiCreatePalette = async (prompt: string) => {
     ],
     temperature: ai_model.temperature ?? 0.5,
   });
-  const res = ai_res.choices?.[0]?.message.content;
+  let res = ai_res.choices?.[0]?.message.content;
+  res = res?.replaceAll("```json", "");
+  res = res?.replaceAll("```", "");
 
   if (!res) {
     throw new Error("Failed to generate palette");
